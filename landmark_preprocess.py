f575597eda9f0048be2fe5b7931538a904e454bc
Imad Toubal <imad.toubal@gmail.com>
2020-07-26 12:37:08 -0500

    nvm

--- landmark_preprocess.py
+++ landmark_preprocess.py
@@ -18,7 +18,7 @@ parser.add_argument('output', help="Saves landmark images to this file path, in 
 args = parser.parse_args()
 total_videos = 0
 count_processed = 0
-MAX_PARALLEL_PROCS = 20
+MAX_PARALLEL_PROCS = 50
 
 '''
 Landmarks from face_alignment are located as follows:
@@ -216,14 +216,14 @@ def main():
 
         total_videos += len(file_list)
 
-    # landmark_args_list = split_list(landmark_args, 20)
-    # for landmark_args in landmark_args_list:
-    with concurrent.futures.ProcessPoolExecutor(mp_context=multiprocessing.get_context('spawn')) as executor:
-        results = [executor.submit(process_video, fa, subfolder, vid)
-                   for subfolder, vid in landmark_args]
+    landmark_args_list = split_list(landmark_args, 20)
+    for landmark_args in landmark_args_list:
+        with concurrent.futures.ProcessPoolExecutor(mp_context=multiprocessing.get_context('spawn')) as executor:
+            results = [executor.submit(process_video, fa, subfolder, vid)
+                       for subfolder, vid in landmark_args]
 
-        for f in concurrent.futures.as_completed(results):
-            print(f.result())
+            for f in concurrent.futures.as_completed(results):
+                print(f.result())
 
     process_time = time.time() - start
     print('PROCESS TIME: {:.3f} h for {} videos (out of {})'.format(
