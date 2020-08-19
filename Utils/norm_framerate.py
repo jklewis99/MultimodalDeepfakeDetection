dc7cb2c63e78377cc1b8e8ac2c6b38868e4e6f20
john jack <jklewis99@gmail.com>
2020-08-18 20:35:38 -0500

    multiprocess

--- Utils/norm_framerate.py
+++ Utils/norm_framerate.py
@@ -1,7 +1,8 @@
 import os
 import argparse
-from misc import create_directory
-import cv2
+import concurrent.futures
+import subprocess
+from misc import create_directory, split_list
 
 parser = argparse.ArgumentParser(description='Convert videos to 30 fps')
 parser.add_argument('input', help="Folder containing fake and real folders, inside those folders \
@@ -13,23 +14,26 @@ parser.add_argument('-fps', default=30, help="intended frame rate")
 def main():
     args = parser.parse_args()
     subfolders = ['real', 'fake']
-    print('response')
+    args_list = []
     for subfolder in subfolders:
         file_list = [video_label for video_label in os.listdir(os.path.join(args.input, subfolder))]
         for video_id in file_list:
             input_path = os.path.join(args.input, subfolder, video_id)
             output_path = create_directory(os.path.join(args.output, subfolder))
-            os.system(f'ffmpeg -i {input_path} -filter:v fps=fps={args.fps} {os.path.join(output_path, video_id)}')
+            args_list.append((input_path, os.path.join(output_path, video_id)))
 
-def test():
-    CV2_FRAMECOUNT_ID = int(cv2.CAP_PROP_FRAME_COUNT)
-    CV2_FPS_ID = int(cv2.CAP_PROP_FPS)
-    video_file = r'J:\reu\code\input\deepfake-detection-challenge\ii\fake\aagfhgtpmv.mp4'
-    cap = cv2.VideoCapture(video_file)
-    framecount = int(cv2.VideoCapture.get(cap, CV2_FRAMECOUNT_ID))
-    fps = cv2.VideoCapture.get(cap, CV2_FPS_ID)
-    cap.release()
-    print(fps)
+    args_list = split_list(args_list, 20)
+    for arg_tuple in args_list:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = [executor.submit(set_fps, input_path, output_path, args.fps)
+                       for input_path, output_path in args_list]
+
+            for f in concurrent.futures.as_completed(results):
+                print(f.result())
+
+def set_fps(input_path, output_path, fps):
+    # os.system(f'ffmpeg -i {input_path} -filter:v fps=fps={args.fps} {os.path.join(output_path, video_id)}')
+    subprocess.check_output(f'ffmpeg -i {input_path} -filter:v fps=fps={fps} {output_path}', shell=True)
 
 if __name__ == '__main__':
-    test()
+    main()
