6120c23e030ec4020a5cd30425d606ee3f5932ca
Imad Toubal <imad.toubal@gmail.com>
2020-09-16 14:16:19 -0500

    audio update

--- Utils/audio.py
+++ Utils/audio.py
@@ -2,6 +2,7 @@ import librosa
 import numpy as np
 from moviepy.editor import VideoFileClip
 
+
 def load_audio(video_path, fps=16000):
     '''
     takes a path to a video file and returns the monophonic audio sample
@@ -9,27 +10,26 @@ def load_audio(video_path, fps=16000):
         video_path: realtive or absolute path to a video file
     '''
     videoclip = VideoFileClip(video_path)
-    # TODO: if audio does not exist, add an empty AudioFileClip object and set
-    # video to that empty audio
     # if videoclip.audio is None:
     #     videoclip.set_audio
-    if videoclip.audio is None:
-        return np.zeros(int(videoclip.duration * fps))
-        
-    audio = videoclip.audio.set_fps(fps).to_soundarray()
+    try:
+        audio = videoclip.audio.set_fps(fps).to_soundarray()
 
-    if len(audio.shape) > 1:
-        if audio.shape[1] == 1:
-            audio = audio.squeeze()
-        else:
-            audio = audio.mean(axis=1)  # multiple channels, average
-    return audio
+        if len(audio.shape) > 1:
+            if audio.shape[1] == 1:
+                audio = audio.squeeze()
+            else:
+                audio = audio.mean(axis=1)  # multiple channels, average
+        return audio
+    except:
+        return np.zeros(int(videoclip.duration * fps))
+
 
 def parse_audio_spect(video_path, win_stride, win_size, sample_rate, window):
     '''
     takes a path to a video file and returns the spectrogram of that video's \
     audio sample and returns the log of that spectrogram
-    
+
     Args:
         win_stride = size of the stride between audio samples
         win_size = size of the window between audio samples
@@ -43,7 +43,8 @@ def parse_audio_spect(video_path, win_stride, win_size, sample_rate, window):
     win_length = n_fft
     hop_length = int(sample_rate * win_stride)
     # STFT
-    D = librosa.stft(sound_array, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window)
+    D = librosa.stft(sound_array, n_fft=n_fft, hop_length=hop_length,
+                     win_length=win_length, window=window)
     spect, phase = librosa.magphase(D)
     # S = log(S+1)
     spect = np.log1p(spect)
